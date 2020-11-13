dd2e814d1f9e12b46bc8bbbfbffecace2afe1ae1
Nicolas MASSART <nicolas.massart@consensys.net>
2020-11-13 16:36:01 +0100

    tag

--- .openapidoc/debug-generate.js
+++ .openapidoc/debug-generate.js
@@ -8,15 +8,22 @@ var crypto = require("crypto")
 
 let spec = require('./spec/teku.json')
 
-console.log(spec.info)
 
-var id = crypto.randomBytes(4).toString('hex')
-var version = spec.info.version.split('-')
-var versionNum = version[0].split('.')
-const parsedReleaseNum = Number.parseInt(versionNum[2], 10)
-versionNum[2] = parsedReleaseNum + 1
-spec.info.version = `${versionNum.join('.')}-${version[1]}-${id}`
+const tag = process.env.CIRCLE_TAG
 
+console.log(tag)
+
+if (tag){
+  spec.info.version = tag
+}
+else {
+  var id = crypto.randomBytes(4).toString('hex')
+  var version = spec.info.version.split('-')
+  var versionNum = version[0].split('.')
+  const parsedReleaseNum = Number.parseInt(versionNum[2], 10)
+  versionNum[2] = parsedReleaseNum + 1
+  spec.info.version = `${versionNum.join('.')}-${version[1]}-${id}`
+}
 console.log(spec.info)
 
 let data = JSON.stringify(spec);
